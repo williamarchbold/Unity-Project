9660ea3e7a57aea3643ddaf9242210e4c7b97b98
williamarchbold <williamsarchbold@gmail.com>
2019-11-09 21:59:43 -0700

    no message

---
+++ Standard Corsi Game/Assets/ClickButton.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+    public Material light_material;
+    public Material normal_material;
+
+    private Renderer my_renderer;
+    private Vector3 my_transform_position;
+
+    public int my_number = 99; //the button's number. default 99 
+
+    public RobotLogic my_logic; 
+
+    public delegate void ClickEV(int number);
+
+    public event ClickEV onClick; 
+
+    void Awake()
+    {
+        my_renderer = GetComponent<Renderer>();
+        my_renderer.enabled = true;
+        my_transform_position = transform.position; //to know where we are at start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (my_logic.player)
+        {
+            ClickedColor();
+            transform.position = new Vector3(my_transform_position.x, -.2f, my_transform_position.z); //changes shape of object clicked by -.3
+            onClick.Invoke(my_number); //tell which button is pressed 
+        }
+        
+    }
+
+    private void OnMouseUp()
+    {
+        UnClickedColor();
+        transform.position = new Vector3(my_transform_position.x, my_transform_position.y, my_transform_position.z); //changes shape of object clicked by -.3
+
+    }
+
+    public void ClickedColor()
+    {
+        my_renderer.sharedMaterial = normal_material;
+    }
+
+    public void UnClickedColor()
+    {
+        my_renderer.sharedMaterial = light_material;
+    }
+}
---
+++ Standard Corsi Game/Assets/RobotLogic.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RobotLogic : MonoBehaviour
+{
+    public ClickButton[] my_buttons;
+    public List<int> color_list; 
+
+    public float show_time = 0.5f;
+    public float pause_time = 0.5f;
+
+    public int level = 2;
+    private int player_level = 0;
+
+    bool robot = false;
+    public bool player = false;
+
+    private int my_random;
+
+    public Button start_button;
+    public Text game_over_text;
+    public Text score_text;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < my_buttons.Length; i++)
+        {
+            my_buttons[i].onClick += ButtonClicked;
+            my_buttons[i].my_number = i; //set 
+        }
+    }
+
+    void ButtonClicked(int _number) 
+    {
+        if (player)
+        {
+            if (_number == color_list[player_level])
+            {
+                player_level += 1;
+                score += 1;
+                score_text.text = score.ToString();
+            }
+            else
+            {
+                GameOver();
+            }
+            if (player_level == level) //no more buttons to press
+            {
+                level += 1; //set level for next time 1 higher
+                player_level = 0; //reset player level for next round
+                player = false;
+                robot = true; //get robot ready for next round 
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (robot)
+        {
+            robot = false;
+            StartCoroutine(Robot());
+        }
+        
+    }
+
+    //wait one seconds, get random value, change color of button @ random value,
+    //wait for show_time seconds, change color back, wait pause_time seconds
+    private IEnumerator Robot()
+    {
+        yield return new WaitForSeconds(1f); //1 sec
+        for (int i = 0; i < level; i++)
+        {
+            if (color_list.Count < level)
+            {
+                my_random = Random.Range(0, my_buttons.Length);
+                color_list.Add(my_random); //store to save color order for later comparison
+            }
+            
+            my_buttons[color_list[i]].ClickedColor();
+            yield return new WaitForSeconds(show_time);
+            my_buttons[color_list[i]].UnClickedColor();
+            yield return new WaitForSeconds(pause_time);
+
+        }
+
+    }
+
+    public void StartGame()
+    {
+        robot = true;
+        score = 0;
+        player_level = 0;
+        level = 2;
+        game_over_text.text = "";
+        score_text.text = score.ToString();
+        start_button.interactable = false;
+
+        player = true; 
+    }
+
+    void GameOver()
+    {
+        game_over_text.text = "Game Over";
+        start_button.interactable = true;
+        player = false;
+    }
+}
